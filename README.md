029307cda0320066c818d9938bd1f462bb2b9853
Heidy Ben Yahia <heidy.benyahia@gmail.com>
2020-10-05 16:53:08 +0200

    optimization + readme

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Usage
+
+For setting the program
+
+`npm install`
+
+For running with a input file
+
+`npm start roller_coaster.hard`
+
--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
-
-import * as fs from 'fs';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
 console.error("Reading file...");
-
 var data = fs.readFileSync(process.argv[2], 'utf8').split('\n');
-
-var inputs: string[] = data[0].split(' ');
-
-const L: number = parseInt(inputs[0]);
-const C: number = parseInt(inputs[1]);
-const N: number = parseInt(inputs[2]);
-const pi: Array<number> = new Array<number>(N);
-
-let n_people: number = 0;
-for (let i = 0; i < N; i++) {
+var inputs = data[0].split(' ');
+var L = parseInt(inputs[0]);
+var C = parseInt(inputs[1]);
+var N = parseInt(inputs[2]);
+var pi = new Array(N);
+var n_people = 0;
+for (var i = 0; i < N; i++) {
     pi[i] = parseInt(data[i + 1]);
     n_people += pi[i];
 }
@@ -21,19 +17,14 @@ console.error("capacity : " + L);
 console.error("number of run per day : " + C);
 console.error("number of groups :" + N);
 console.error("number of people :" + n_people);
-
-
-let index: number = 0;
-let cach: Array<[number, number]> = new Array<[number, number]>(N);
-
+var index = 0;
+var cach = new Array(N);
 console.error("Start computing result...");
-
-let total: number = 0;
-let p: number = 0;
-
-for (let i = 0; i < C; i++) {
-    let charge: number = 0;
-    let firstgroup: number = index;
+var total = 0;
+var p = 0;
+for (var i = 0; i < C; i++) {
+    var charge = 0;
+    var firstgroup = index;
     if (cach[firstgroup]) {
         console.error("retrieve from caching...");
         total += cach[firstgroup][1];
@@ -46,10 +37,9 @@ for (let i = 0; i < C; i++) {
             index = (index + 1 < N ? index + 1 : 0);
         }
         cach[firstgroup] = [index, charge];
-        ++p;        
+        ++p;
         console.log("comput");
     }
     console.error("current earnings : " + total);
 }
-
 console.log(total);
